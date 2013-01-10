2f789a5d1289d17dabc7e3e2c4c3b0d7b3d3c541
Daniel Ge <dange@seas.upenn.edu>
2013-01-10 02:34:31 -0500

    Load redis.yml through ERB, and then YAML
    Allows us to use ERB-style substitutions in the redis YAML file, and
    thus add environmental variables.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 # Load the redis config from the config/redis.yml file and expose redis as the
 # global $redis
-$redis = Redis.new(YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env])
+path = "#{Rails.root}/config/redis.yml"
+$redis = Redis.new(YAML.load(ERB.new(File.new(path).read).result)[Rails.env])
