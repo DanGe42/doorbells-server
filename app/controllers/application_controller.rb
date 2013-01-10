0ffb9128189b1029c68bc1b05c2e3e922f5bb106
Daniel Ge <dange@seas.upenn.edu>
2013-01-10 02:32:55 -0500

    Accept more options in json_st...sponse in application
    Allow the json_status_response to take an additional Hash to merge with
    the returned default JSON. This allows us to extend this helper method
    easily.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,32 @@ class ApplicationController < ActionController::Base
   protected
   # Usage:
   #   render json_status_response(400, "message")
-  def json_status_response(status, message)
+  #
+  # Examples:
+  #   render json_status_response(404, "Not found")
+  #   == render :status => 404,
+  #             :json => {
+  #               status: 404,
+  #               msg: "Not found"
+  #             }
+  #
+  #   render json_status_response(404, "Not found", { nyan: "cat" })
+  #   == render :status => 404,
+  #             :json => {
+  #               status: 404,
+  #               msg: "Not found",
+  #               nyan: "cat"
+  #             }
+  def json_status_response(status, message, more={})
+    json = {
+      status: status,
+      msg: message
+    }
+    json.merge!(more)
+
     return {
       :status => status,
-      :json => {
-        status: status,
-        msg: message,
-      }
+      :json => json
     }
   end
 end
