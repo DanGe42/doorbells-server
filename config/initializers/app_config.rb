5751662b9e64eb23705966497745eaa51a23b221
Daniel Ge <dange@seas.upenn.edu>
2013-01-11 17:22:05 -0500

    Update config YAML files with env variables
    And also, use CAPITAL LETTER variables.

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,2 +1,3 @@
 # Load configuration from the config/config.yml file and expose as APP_CONFIG
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+path = "#{Rails.root}/config/config.yml"
+APP_CONFIG = YAML.load(ERB.new(File.new(path).read).result)[Rails.env]
